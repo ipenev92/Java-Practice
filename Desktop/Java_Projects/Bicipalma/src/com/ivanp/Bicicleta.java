515ae3ec97fab74bbcc564ada18f7da51ae93433
Ivan Penev <ivan.penev92@gmail.com>
2018-09-03 16:24:27 +0200

    getters, setters, constructor

--- Desktop/Java_Projects/Bicipalma/src/com/ivanp/Bicicleta.java
+++ Desktop/Java_Projects/Bicipalma/src/com/ivanp/Bicicleta.java
@@ -1,4 +1,17 @@
 package com.ivanp;
 
 public class Bicicleta {
+    private int id;
+
+    public Bicicleta(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
--- Desktop/Java_Projects/Bicipalma/src/com/ivanp/TarjetaUsuario.java
+++ Desktop/Java_Projects/Bicipalma/src/com/ivanp/TarjetaUsuario.java
@@ -1,4 +1,27 @@
 package com.ivanp;
 
 public class TarjetaUsuario {
+    private String id;
+    private boolean activada;
+
+    public TarjetaUsuario(String id, boolean activada) {
+        this.id = id;
+        this.activada = activada;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public boolean isActivada() {
+        return activada;
+    }
+
+    public void setActivada(boolean activada) {
+        this.activada = activada;
+    }
 }
