6b695aeda1820d174e49f68fb17a70e2b51620ea
Ivan Penev <ivan.penev92@gmail.com>
2018-09-03 16:42:24 +0200

    finished

--- Desktop/Java_Projects/Bicipalma/src/com/ivanp/Estacion.java
+++ Desktop/Java_Projects/Bicipalma/src/com/ivanp/Estacion.java
@@ -1,5 +1,7 @@
 package com.ivanp;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Estacion {
     private int id;
     private String direccion;
@@ -59,5 +61,70 @@ public class Estacion {
         }
 
         return anclajes;
+    }
+
+    public void consultarAnclajes() {
+        int anclajes = 1;
+
+        for (Bicicleta ancl : this.anclaje) {
+            if (ancl != null) {
+                System.out.println("El anclaje numero " + this.anclaje[anclajes].getId() + " ocupado.");
+            } else {
+                System.out.println("El anclaje numero " + this.anclaje[anclajes].getId() + " libre.");
+            }
+
+            anclajes++;
+        }
+    }
+
+    public void anclarBicicleta(Bicicleta bici) {
+        int anclajes = 1;
+
+        for (Bicicleta ancl : this.anclaje) {
+            if (ancl == null) {
+                this.anclaje[anclajes] = bici;
+                System.out.println("Bicicleta anclada");
+                break;
+            } else {
+                anclajes++;
+            }
+        }
+    }
+
+    public void mostrarAnclaje(Bicicleta bici, int numeroAnclajes) {
+        System.out.println("Retirada: " + bici.getId() + ", anclaje: " + numeroAnclajes);
+    }
+
+    public boolean leerTarjetaUsuario(TarjetaUsuario tarjeta) {
+        if (tarjeta.isActivada()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void retirarBicicleta(TarjetaUsuario tarjeta) {
+        if (leerTarjetaUsuario(tarjeta)) {
+            while (true) {
+                int pos = generarAnclaje();
+                int anclaje = pos + 1;
+
+                if (this.anclaje[pos] != null) {
+                    mostrarBicicleta(this.anclaje[pos], numeroAnclajes);
+                    this.anclaje[pos] = null;
+                    break;
+                }
+            }
+        } else {
+            System.out.println("Tarjeta: false");
+        }
+    }
+
+    public void mostrarBicicleta(Bicicleta bici, int numeroAnclajes) {
+        System.out.println("Anclada: " + bici.getId() + ", anclaje: " + numeroAnclajes);
+    }
+
+    public int generarAnclaje(){ // a hardware anclaje
+        return ThreadLocalRandom.current().nextInt(0, this.anclaje.length);
     }
 }
